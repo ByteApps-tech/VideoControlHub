dd34e04fb61a74c3663f62d61accb987dc9ca78c
cx33 <lne128gene980@163.com>
2023-07-09 18:45:25 +0800

    feat: input validate

--- popup.js
+++ popup.js
@@ -19,6 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const speedSubtractKeyEl = document.getElementById('speedSubtractKey')
     speedSubtractKeyEl.value = speedSubtractKey || 'a'
     speedSubtractKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = speedSubtractKey || 'a'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, speedSubtractKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -27,6 +31,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const speedAddKeyEl = document.getElementById('speedAddKey')
     speedAddKeyEl.value = speedAddKey || 'd'
     speedAddKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = speedAddKey || 'd'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, speedAddKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -35,6 +43,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const speedResetKeyEl = document.getElementById('speedResetKey')
     speedResetKeyEl.value = speedResetKey || 's'
     speedResetKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = speedResetKey || 's'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, speedResetKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -43,6 +55,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const speedStepEl = document.getElementById('speedStep')
     speedStepEl.value = speedStep || 0.1
     speedStepEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = speedStep || 0.1
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, speedStep: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -51,6 +67,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const volumeSubtractKeyEl = document.getElementById('volumeSubtractKey')
     volumeSubtractKeyEl.value = volumeSubtractKey || 'z'
     volumeSubtractKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = volumeSubtractKey || 'z'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, volumeSubtractKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -59,6 +79,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const volumeAddKeyEl = document.getElementById('volumeAddKey')
     volumeAddKeyEl.value = volumeAddKey || 'c'
     volumeAddKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = volumeAddKey || 'c'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, volumeAddKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -67,6 +91,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const volumeResetKeyEl = document.getElementById('volumeResetKey')
     volumeResetKeyEl.value = volumeResetKey || 'x'
     volumeResetKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = volumeResetKey || 'x'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, volumeResetKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -75,6 +103,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const volumeStepEl = document.getElementById('volumeStep')
     volumeStepEl.value = volumeStep || 0.1
     volumeStepEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = volumeStep || 0.1
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, volumeStep: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -83,6 +115,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const volumeDefaultEl = document.getElementById('volumeDefault')
     volumeDefaultEl.value = volumeDefault || 0.5
     volumeDefaultEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = volumeDefault || 0.5
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, volumeDefault: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -91,6 +127,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const forwardKeyEl = document.getElementById('forwardKey')
     forwardKeyEl.value = forwardKey || 'w'
     forwardKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = forwardKey || 'w'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, forwardKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -99,6 +139,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const backwardKeyEl = document.getElementById('backwardKey')
     backwardKeyEl.value = backwardKey || 'q'
     backwardKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = backwardKey || 'q'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, backwardKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -107,6 +151,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const forwardStepEl = document.getElementById('forwardStep')
     forwardStepEl.value = forwardStep || 10
     forwardStepEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = forwardStep || 10
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, forwardStep: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
@@ -115,9 +163,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const trunLightKeyEl = document.getElementById('trunLightKey')
     trunLightKeyEl.value = trunLightKey || 'e'
     trunLightKeyEl.addEventListener('change', e => {
+      if (!checkValue(e.target.value, result.config)) {
+        e.target.value = trunLightKey || 'e'
+        return
+      }
       chrome.storage.sync.set({ config: { ...result.config, trunLightKey: e.target.value } }, () => {
         console.log('Value is set to ' + e.target.value);
       });
     })
   });
 });
+
+
+function checkValue(value, config) {
+  if (value.length !== 1) {
+    alert('快捷键只能是一个字符')
+    return false
+  }
+  value = value.toLowerCase()
+  if (Object.values(config).includes(value)) {
+    alert('快捷键不能重复')
+    return false
+  }
+  return true
+}
